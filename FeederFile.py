2452cbb1f13d3aeb27389037eed1fa8bbd2dd906
Harlan Howe <harlan.howe@kinkaid.org>
2025-02-21 16:29:16 -0600

    oops. flipped max and min when constraining speeds and turn ratios. gave default speed no longer clearing speed and turn_ratio.

--- FeederFile.py
+++ FeederFile.py
@@ -20,7 +20,7 @@ class Feeder:
         self.position: List[float] = [random.randint(0, 800), random.randint(0, 800)]
         print(f"{self.position=}")
         self.orientation = random.random()*2*math.pi-math.pi
-        self.speed = 0.0
+        self.speed = 15.0
         self.turn_ratio = 0.0
         self.food_sensors = [0.0 for i in range(NUM_SENSORS)]
         self.danger_sensors = [0.0 for i in range(NUM_SENSORS)]
@@ -35,16 +35,18 @@ class Feeder:
         if display_sensors:
             for i in range(NUM_SENSORS):
                 angle = (i * math.pi * 2 / NUM_SENSORS + self.orientation) % (2*math.pi) - math.pi
-                cv2.line(img=canvas, pt1=(int(self.position[0]), int(self.position[1])),
-                         pt2=(int(self.position[0] + DANGER_SENSOR_RADIUS * math.cos(angle)),
-                              int(self.position[1] + DANGER_SENSOR_RADIUS * math.sin(angle))),
-                         color=(0.5, 0.5+self.danger_sensors[i]/2, 0.5),
-                         thickness=1)
-                cv2.line(img=canvas, pt1=(int(self.position[0]), int(self.position[1])),
-                         pt2=(int(self.position[0] + FOOD_SENSOR_RADIUS * math.cos(angle)),
-                              int(self.position[1] + FOOD_SENSOR_RADIUS * math.sin(angle))),
-                         color=(0.0, 0.0, self.food_sensors[i]),
-                         thickness=1)
+                if self.danger_sensors[i]>0:
+                    cv2.line(img=canvas, pt1=(int(self.position[0]), int(self.position[1])),
+                             pt2=(int(self.position[0] + DANGER_SENSOR_RADIUS * math.cos(angle)),
+                                  int(self.position[1] + DANGER_SENSOR_RADIUS * math.sin(angle))),
+                             color=(0.5, 0.5+self.danger_sensors[i]/2, 0.5),
+                             thickness=1)
+                if self.food_sensors[i]>0:
+                    cv2.line(img=canvas, pt1=(int(self.position[0]), int(self.position[1])),
+                             pt2=(int(self.position[0] + FOOD_SENSOR_RADIUS * math.cos(angle)),
+                                  int(self.position[1] + FOOD_SENSOR_RADIUS * math.sin(angle))),
+                             color=(0.0, 0.0, self.food_sensors[i]),
+                             thickness=1)
 
         cv2.circle(img=canvas, center=(int(self.position[0]), int(self.position[1])), radius=FEEDER_RADIUS, color=self.color,
                    thickness=-1)
@@ -84,22 +86,25 @@ class Feeder:
             self.danger_sensors[index] = max(self.danger_sensors[index], proximity)
         else:
             self.food_sensors[index] = max(self.food_sensors[index],proximity)
+        # print(f"{self.danger_sensors=}\t{self.food_sensors=}")
 
     def animation_step(self, delta_t:float):
-        self.speed = 0
-        self.turn_ratio = 0
+        # self.speed = 0
+        # self.turn_ratio = 0
         for i in range(NUM_SENSORS):
             self.speed += (self.genes[i] * self.food_sensors[i] +
                            self.genes[i+NUM_SENSORS] * self.danger_sensors[i])
             self.turn_ratio += (self.genes[i+NUM_SENSORS*2] * self.food_sensors[i] +
                                 self.genes[i+NUM_SENSORS*3] * self.danger_sensors[i])
-        self.speed = max(MAX_SPEED, min(-MAX_SPEED, self.speed))
-        self.turn_ratio = max(MAX_TURN_RATIO, min(-MAX_TURN_RATIO, self.turn_ratio))
+        self.speed = min(MAX_SPEED, max(-MAX_SPEED, self.speed))
+        self.turn_ratio = min(MAX_TURN_RATIO, max(-MAX_TURN_RATIO, self.turn_ratio))
 
         self.orientation += self.turn_ratio*delta_t/2
-        self.position = [self.position[0]+self.speed*delta_t*math.cos(self.orientation),
+        self.position = [self.position[0]+self.speed * delta_t * math.cos(self.orientation),
                          self.position[1]+self.speed * delta_t * math.sin(self.orientation)]
         self.orientation += self.turn_ratio * delta_t / 2
+
+        # print(f"{self.speed=}\t{self.turn_ratio=}")
 
 
 
